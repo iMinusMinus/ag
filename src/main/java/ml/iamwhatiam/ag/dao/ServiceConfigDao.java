1cae027f45898803b73e2cc119eceabd351439e0
iMinusMinus <mean.leung@outlook.com>
2017-10-09 16:06:06 +0800

    fix sql

--- src/main/java/ml/iamwhatiam/ag/dao/ServiceConfigDao.java
+++ src/main/java/ml/iamwhatiam/ag/dao/ServiceConfigDao.java
@@ -45,30 +45,28 @@ import ml.iamwhatiam.ag.domain.MethodDomain;
 @Repository("serviceConfig")
 public interface ServiceConfigDao {
 
-    @Insert("INSERT INTO service (service_name, interface_name, method_name, rococo, return_type "
-    		+ "VALUES (#{serviceName}, #{interfaceName}, #{methodName}, #{rococo}, #{returnType})")
+    @Insert("INSERT INTO service (service_name, interface_name, method_name, rococo, return_type) "
+            + "VALUES (#{serviceName}, #{interfaceName}, #{methodName}, #{rococo}, #{returnType})")
     /* oracle before, mysql after */
     @SelectKey(statement = {
             "SELECT LAST_INSERT_ID() AS ID" }, keyProperty = "id", before = false, resultType = long.class)
     long save(MethodDomain method);
-    
+
     @Update("<script>UPDATE service <trim prefix = \"SET\" suffixOverrides = \",\">"
-    		+ "<if test=\"serviceName != null\">service_name = #{serviceName}</if>"
-    		+ "<if test=\"interfaceName != null\">interface_name = interfaceName</if>"
-    		+ "<if test=\"methodName != null\">method_name = #{methodName}</if>"
-    		+ "<if test=\"rococo != null\">rococo = rococo</if>"
-    		+ "<if test=\"returnType != null\">return_type = returnType</if></trim> "
-    		+ "WHERE id = #{id}")
+            + "<if test=\"serviceName != null\">service_name = #{serviceName}</if>"
+            + "<if test=\"interfaceName != null\">interface_name = interfaceName</if>"
+            + "<if test=\"methodName != null\">method_name = #{methodName}</if>"
+            + "<if test=\"rococo != null\">rococo = rococo</if>"
+            + "<if test=\"returnType != null\">return_type = returnType</if></trim> " + "WHERE id = #{id}</script>")
     int update(MethodDomain domain);
-    
+
     @Delete("DELETE FROM service WHERE id = #{id}")
     int delete(long id);
 
     @Results({ @Result(id = true, column = "id", property = "id"),
             @Result(column = "service_name", property = "serviceName"),
             @Result(column = "interface_name", property = "interfaceName"),
-            @Result(column = "method_name", property = "methodName"),
-            @Result(column = "rococo", property = "rococo"),
+            @Result(column = "method_name", property = "methodName"), @Result(column = "rococo", property = "rococo"),
             @Result(column = "return_type", property = "returnType"),
             @Result(column = "id", property = "parameters", many = @Many(select = "ml.iamwhatiam.ag.dao.ParamConfigDao.findByMethodId", fetchType = FetchType.LAZY)) })
     @Select("SELECT * FROM service s WHERE s.service_name = #{serviceName}")
